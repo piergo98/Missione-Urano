07d8ba35697586eaf0d31b994479543dac632cbe
Pietro Gori <p.gori3@studenti.unipi.it>
2022-03-14 19:34:21 +0100

    Aggiunto il file di escape dalla Terra

--- Earth_parking_orbit.m
+++ Earth_parking_orbit.m
@@ -6,7 +6,7 @@ lat = 5.2;  % latitude of Kourou base (deg)
 
 beta = asind(cosd(i) / cosd(lat));     % azimuth necesary for the launch
 
-r_orbit = 6571;     % Earth radius (km)
+r_orbit = 6571;     % Earth parking orbit radius (km)
 mu_e = 3.986*10^5;  % mu Earth (km^3/s^2)
 
 v_orbit = sqrt(mu_e / r_orbit);     % orbit velocity (km/s)
