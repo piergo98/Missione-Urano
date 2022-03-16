08d1edac71f08a839642d02514fc114d3baece6e
Pietro Gori <p.gori3@studenti.unipi.it>
2022-03-16 12:19:27 +0100

    Modifiche di indice

--- Lambert_Jupiter2Saturn.m
+++ Lambert_Jupiter2Saturn.m
@@ -37,24 +37,24 @@ deg = pi/180;
 
 mu = 1.327*10^11;                   % mu sun (km^3/s^2)
 % Position of Jupiter at the departure (km)
-[coe1, r1, v1, jd1] = planet_elements_and_sv(5, 2026, 06, 09, 12, 00, 00);
+[coe1_j, r1_j, v1_j, jd1_j] = planet_elements_and_sv(5, 2026, 06, 09, 12, 00, 00);
 % Position of Saturn at the arrival  (km)     
-[coe2, r2, v2, jd2] = planet_elements_and_sv(6, 2028, 07, 27, 12, 00, 00);
+[coe2_s, r2_s, v2_s, jd2_s] = planet_elements_and_sv(6, 2028, 07, 27, 12, 00, 00);
 % TOF (s)
 dt     = 67305600;                   
 string = 'pro';
 %...
 
 %...Algorithm 5.2:
-[v1_l, v2_l] = lambert(r1, r2, dt, string);
+[v1_l_j, v2_l_s] = lambert(r1_j, r2_s, dt, string);
 
 %...Algorithm 4.1 (using r1 and v1):
-coe      = coe_from_sv(r1, v1_l, mu);
+coe      = coe_from_sv(r1_j, v1_l_j, mu);
 %...Save the initial true anomaly:
 TA1      = coe(6);
 
 %...Algorithm 4.1 (using r2 and v2):
-coe      = coe_from_sv(r2, v2_l, mu);
+coe      = coe_from_sv(r2_s, v2_l_s, mu);
 %...Save the final true anomaly:
 TA2      = coe(6);
 
@@ -64,16 +64,16 @@ fprintf('\n Lambert''s Problem from Jupiter to Saturn\n')
 fprintf('\n\n Input data:\n');
 fprintf('\n   Gravitational parameter (km^3/s^2) = %g\n', mu);
 fprintf('\n   r1 (km)                       = [%g  %g  %g]', ...
-                                            r1(1), r1(2), r1(3))
+                                            r1_j(1), r1_j(2), r1_j(3))
 fprintf('\n   r2 (km)                       = [%g  %g  %g]', ...
-                                            r2(1), r2(2), r2(3))
+                                            r2_s(1), r2_s(2), r2_s(3))
 fprintf('\n   Elapsed time (s)              = %g', dt);
 fprintf('\n\n Solution:\n')
 
 fprintf('\n   v1 (km/s)                     = [%g  %g  %g]', ...
-                                            v1_l(1), v1_l(2), v1_l(3))
+                                            v1_l_j(1), v1_l_j(2), v1_l_j(3))
 fprintf('\n   v2 (km/s)                     = [%g  %g  %g]', ...
-                                            v2_l(1), v2_l(2), v2_l(3))
+                                            v2_l_s(1), v2_l_s(2), v2_l_s(3))
 																							 
 fprintf('\n\n Orbital elements:')
 fprintf('\n   Angular momentum (km^2/s)     = %g', coe(1))
@@ -96,4 +96,4 @@ if coe(2)<1
 end
 fprintf('\n-----------------------------------------------------\n')		
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-orbit_Jupiter2Saturn(r1, v1_l)
+orbit_Jupiter2Saturn(r1_j, v1_l_j)
