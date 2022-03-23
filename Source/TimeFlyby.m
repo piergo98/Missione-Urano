54812c34da6f15299e26ce7ad188a1c6278a0c9b
Lorenzo Lehmann <l.lehmann@studenti.unipi.it>
2022-03-23 18:29:37 +0100

    aggiornamento, ancora con dei problemi

--- Source/TimeFlyby.m
+++ Source/TimeFlyby.m
@@ -1,33 +1,55 @@
-%Calcolo il tempo t di flyby(CI SI PROVA) usando l'equazione di keplero
+%Calcolo il tempo t dio flyby(CI SI PROVA) usando l'equazione di keplero
 
 %test
-a = 10000;
-e = 0.5;
-r_p = 14147;
+%  a = 10000;
+%  e = 0.5;
+%  r_in = 14147;
+%  r_p = a*(1-e);
+r_SOI_Jupiter = 48.2e6;
+%conosco a,e,r_p dallo script Flyby_jupiter
 
-%conosco a,e,r dallo script Flyby_jupiter
+%r_in = p/(1+e*cos(f))
+%p = r_in*(1+e*cos(f))
+%ricavo l'anomalia vera f (di ingresso SOI?)
 
-%ricavo l'anomalia vera f
+r_in = r_SOI_Jupiter;
 
-f = acosd((a*(1-e^2)-r_p) / (e*r_p))
+%%versione in gradi che non torna
+%cosf = (a*(1-e^2)-r_in) / (e*r_in)
+%f = acosd((a*(1-e^2)-r_in) / (e*r_in))
 
-%trovo l'anomalia eccentrica E
+%p = r_in*(1+e*cosd(f))
+%p2 = r_in*(1+e*cosf)
 
-%tan(E/2)=(((1-e)/(1+e))^(1/2))*(tan(f/2));
+%%versione in radianti di p (NON DOVREBBE FUNZIONARE MA INVECE SEMBRA
+%%ANDARE PER SBAGLIO
+f = acosd((a*(1-e^2)-r_in) / (e*r_in))
+p = r_in*(1+e*cos(f))
 
-E = 2*atand((((1-e)/(1+e))^(1/2))*(tan(f/2)))
+f1 = acosd((1/e)-(r_in/p))
+f2 = -f1
+%f1 = acosd((1/e)-(r_in/p))
+%trovo l'anomalia eccentrica E2
 
-mu = -2*a*E;
+E2 = atanh(((e-1)/(e+1))^(1/2) * tan(f2/2))
+
+%E = 2*atand((((1-e)/(1+e))^(1/2))*(tand(f/2)))
 
 %trovo anomalia media M
 
-M = E - e*sin(E)
+%M = E - e*sin(E)
+
+M2 = e * sinh(E2) - E2
 
 %trovo mean motion n
 
-n = (mu/a^3)^(1/2)
+%n = (mu/a^3)^(1/2)
+%n = (GM_jupiter/a^3)^(1/2)
 
 %trovo il tempo
 
-t_flyby = M/n
+%t_flyby = M/n
 
+t2 = M2 * (((-a^3)/(GM_jupiter))^(1/2))
+
+delta_t = 2 * t2
