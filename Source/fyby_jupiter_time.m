a952d6658b5b9abef1815e3c5bb2786db3de67e5
Francesco Iotti <f.iotti@studenti.unipi.it>
2022-03-28 16:15:51 +0200

    flyby giove

--- Source/fyby_jupiter_time.m
+++ Source/fyby_jupiter_time.m
@@ -5,10 +5,10 @@ v_inf_down_Jupiter = v2_l_j - v2_j ;
 v_inf_down_norm_Jupiter = norm(v_inf_down_Jupiter); 
 
 a_flyby_Jupiter = - GM_jupiter/((v_inf_down_norm_Jupiter)^2);%semiaxis major 
-r_p_flyby_Jupiter = 1e6;  %hp  
+r_p_flyby_Jupiter = 503800;  %hp  
 e_flyby_Jupiter = 1-(r_p_flyby_Jupiter/a_flyby_Jupiter); 
 
-delta_Jupiter = 2*asind(1/e_flyby_Jupiter); %angolo tra gli asintoti 
+delta_Jupiter = 2*asin(1/e_flyby_Jupiter); %angolo tra gli asintoti 
 delta_deg_Jupiter = rad2deg(delta_Jupiter); 
 
 %ricavo l'anomalia vera f
@@ -22,9 +22,9 @@ p_Jupiter = r_p_flyby_Jupiter*(1+e_flyby_Jupiter*cos(f_Jupiter));
 r=R_SOI_Jupiter; 
 
 %calcolo anomalia vera per r 
-f1_Jupier = acos((1/e_flyby_Jupiter)*((p_Jupiter/r)-1)); 
+f1_Jupiter = acos((1/e_flyby_Jupiter)*((p_Jupiter/r)-1)); 
 
-E_2_Jupiter = atanh(sqrt((e_flyby_Jupiter-1)/(e_flyby_Jupiter+1))*tan(f1_Jupier/2)); 
+E_2_Jupiter = atanh(sqrt((e_flyby_Jupiter-1)/(e_flyby_Jupiter+1))*tan(f1_Jupiter/2)); 
 E_Jupiter = E_2_Jupiter/2;
 E_deg_Jupiter = rad2deg(E_Jupiter); 
 
