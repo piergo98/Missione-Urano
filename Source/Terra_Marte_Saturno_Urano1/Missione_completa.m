24d1df2bf33d349faae62acde6e1da6d83501874
Lorenzo Lehmann <l.lehmann@studenti.unipi.it>
2022-05-17 11:46:10 +0200

    Sistemato alcuni script di Marte, non torna la  Solar System però

---
+++ Source/Terra_Marte_Saturno_Urano1/Missione_completa.m
@@ -0,0 +1,54 @@
+%% MISSIONE COMPLETA: Terra-Marte-Saturno-Urano
+
+    % Inizializzo la missione
+    init_Terra_Marte_Saturno_Urano;
+
+    % Calcolo le Traiettorie di Lambert
+    Earth_Mars_Saturn_Uranus;
+
+%% Fase Terra-Marte
+
+    fprintf("\n Partenza dall'orbita di parcheggio sulla Terra il 2022/09/01 ore 18:00:00 \n");
+
+    % Cambio Piano da Equatoriale ad Eclittica
+    Earth_plane_shift;
+
+    % Fuga dalla SOI della Terra
+    Escape_from_Earth;
+
+%% Fase Marte-Saturno
+
+    fprintf('\n Ingresso nella sfera di influenza di Venere il 2023/10/01 ore 18:00:00\n');
+
+    % Fly-by su Marte
+    flyby_Mars_time;
+    
+%% Fase Marte-Saturno
+
+    [years, months, days, hours, minutes, seconds] = sec2date(dT_post_flyby);
+    fprintf('\n Continuo su una traiettoria eliocentrica per %g mesi, %g giorni, %g ore, %g minuti \n',...
+        months,days,hours,minutes);
+    fprintf('\n--------------------------------------------------------\n\n')
+
+    fprintf('\n Ingresso nella sfera di influenza di Saturno il 2028/10/01 ore 00:00:00\n');
+
+    % Fly-by su Saturno
+    flyby_Saturn_time;
+
+%% Flight totale
+
+    Solar_System_animation;
+
+%% Fase Saturno-Urano
+
+    fprintf('\n Ingresso nella sfera di influenza di Urano il 2034/10/01 ore 00:00:00');
+
+    % Cattura su Urano
+    Uranus_capture;
+    
+    % Cambio piano da Eclittica a Equatoriale
+    Uranus_orbital_plane_shift;
+
+%% Cacolo Delta Velocita' totale
+
+    DeltaV;
---
+++ Source/Terra_Marte_Saturno_Urano1/Script matlab/time_post_flyby.m
@@ -0,0 +1,32 @@
+% Funzione per il calcolo del tempo di volo su traiettoria ellittica a
+% seguito dell'uscita dalla SOI di un pianeta dopo flyby
+
+% f1 = anomalia vera nel punto in cui lo spacecraft esce dalla SOI
+% f2 = anomalia vera nel punto in cui avverrà il delta V
+% a = semiasse maggiore orbita ellittica
+% e = eccentricità dell'orbita
+% mu = mu pianeta/stella attorno a cui ruota l'orbita
+
+function dT = time_post_flyby(f1, f2, a, e, mu_Sun)
+    % Tangente anomalia eccentrica in uscita dalla SOI
+    tg_E1 = sqrt((1-e) / (1+e)) * tan(f1/2);
+    % Anomalia eccentrica
+    E1 = 2 * atan(tg_E1);
+    % Anomalia media
+    M1 = E1 - e*sin(E1);
+    % Mean motion
+    n = sqrt(mu_Sun/a^3);
+    % Tempo di volo dal perielio fino all'uscita dalla SOI (solo per i
+    % calcoli)
+    t1 = M1 / n;
+    % Tangente anomalia eccentrica nel punto in cui avverrà il delta V
+    tg_E2 = sqrt((1-e) / (1+e)) * tan(f2/2);
+    % Anomalia eccentrica
+    E2 = 2 * atan(tg_E2);
+    % Anomalia media
+    M2 = E2 - e*sin(E2);
+    % Tempo di volo dal perielio fino al punto in cui avverrà il delta V
+    t2 = M2 / n;
+    % Calcolo il delta T
+    dT = t2 - t1;
+end
