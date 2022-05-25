1bbcb35359202770eb2e71c6d85b0126372d2cb8
Francesco Iotti <f.iotti@studenti.unipi.it>
2022-05-25 12:25:18 +0200

    rimossa terra marte saturno urano inutile

--- Source/Terra_Marte_Saturno_Urano/Script matlab/circle_plot.m
+++
@@ -1,7 +0,0 @@
-% Definisco la funzione che disegna un cerchio nella posizione desiderata
-function h = circle_plot(x,y,r)
-    d = r*2;
-    px = x-r;
-    py = y-r;
-    h = rectangle('Position',[px py d d],'Curvature',[1,1]);
-end
--- Source/Terra_Marte_Saturno_Urano/Script matlab/month2seconds.m
+++
@@ -1,3 +0,0 @@
-function sec = month2seconds(month)
-    sec = 30*24*3600*month;
-end
